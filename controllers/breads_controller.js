4b839f16ea86699329c80f5db3bcd160ef5224f8
Abdulaziz Ali <guuree1982@gmail.com>
2022-07-09 13:35:32 -0400

    edit and update route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,15 @@ breads.get('/', (req, res) => {
    res.send(Bread)
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -34,6 +43,19 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+ // UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
 
 
 
